00c9e70f0dc94643010a85e80214d0cabb5dfc93
Alexey <bystrovalexey15@mail.ru>
2024-10-21 01:38:15 +0700

    tests for new modues, and formaing code

--- src/utils.py
+++ src/utils.py
@@ -4,13 +4,16 @@ import json
 def file_opening(filename: str) -> list[dict]:
     """ Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях """
     empty_list_of_transaction = []
-    with open(filename, "r", encoding="utf-8") as file:
-        try:
-            transactions = json.load(file)
-            if transactions == 0 or type(transactions) is not list:
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            try:
+                transactions = json.load(file)
+                if transactions == 0 or type(transactions) is not list:
+                    return empty_list_of_transaction
+                else:
+                    return transactions
+            except json.JSONDecodeError:
+                print("Invalid JSon data")
                 return empty_list_of_transaction
-            else:
-                return transactions
-        except json.JSONDecodeError:
-            print("Invalid JSon data")
-            return empty_list_of_transaction
+    except FileNotFoundError:
+        return []
