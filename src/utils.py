b4bf352bf1a2abe642f331f852c7065b13331b73
Alexey <bystrovalexey15@mail.ru>
2024-10-25 03:15:14 +0700

    logging for masks.py

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import os
 
 logger = logging.getLogger('utils')
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('../logs/utils.log', mode='w', encoding='utf-8')
+file_handler = logging.FileHandler('logs/utils.log', mode='w', encoding='utf-8')
 file_formater = logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s")
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
@@ -34,7 +34,3 @@ def file_opening(filename: str) -> list[dict]:
     except FileNotFoundError:
         logger.error('Ошибка, файл не найден')
         return []
-
-
-file = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "operations.json")
-file_opening(file)
