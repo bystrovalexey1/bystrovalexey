4b945d6425c4c1300f14e0b30fe6035effccd012
Alexey <bystrovalexey15@mail.ru>
2024-11-11 03:30:52 +0700

    main.py

--- src/widget.py
+++ src/widget.py
@@ -3,7 +3,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(card_or_account_info: str) -> str:
     """Функция, которая маскирует и номер карты, и номер счета"""
-    split_card_or_account_info = card_or_account_info.split(" ")
+    split_card_or_account_info = card_or_account_info.split()
     hidden_account_or_card_number = ""
     if split_card_or_account_info[-1].isdigit() is True and (
         len(split_card_or_account_info[-1]) == 16 or len(split_card_or_account_info[-1]) == 20
